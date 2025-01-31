c9f551352e4b12ff451cdc5d876753f0342a3473
alexandr-prokhorov <prohorovalex0013@gmail.ru>
2025-01-31 22:19:53 +0400

    Задание откорректировал!!!

--- Task_2/main.py
+++ Task_2/main.py
@@ -10,17 +10,17 @@ if __name__ == '__main__':
     book1.edit_information_book("Атлант расправил плечи", "Айн Рэнд", "1957")
     print()
     # Сохраняем информацию в формате через JSON
-    json_handler = Json()
-    json_handler.save_data(book1, "book.json")
+    json = Json()
+    json.save_data(book1, "book.json")
     # Загружаем информацию из файла формата JSON
-    loaded_book_json = json_handler.load_data("book.json")
+    loaded_book_json = json.load_data("book.json")
     # Выводим информацию.
     loaded_book_json.display_book()
     print()
     # Сохраняем информацию в формате через Pickle.
-    pickle_handler = Pickle()
-    pickle_handler.save_data(book1, "book.pkl")
+    pickle = Pickle()
+    pickle.save_data(book1, "book.pkl")
     # Загружаем информацию из файла формата Pickle.
-    loaded_book_pickle = pickle_handler.load_data("book.pkl")
+    loaded_book_pickle = pickle.load_data("book.pkl")
     # Выводим информацию.
     loaded_book_pickle.display_book()
